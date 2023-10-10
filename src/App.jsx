23feb35888c08c7375f3253c6f24f9e0c96f2f43
DrantDumani <darnjames@gmail.com>
2023-10-10 10:38:28 -0400

    Complete light and dark mode for navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.scss";
 import { Navbar } from "./components/Navbar/Navbar";
 import { TimerContainer } from "./components/TimerContainer/TimerContainer";
 
 function App() {
   const [displaySettings, setDisplaySettings] = useState(false);
+  const [mode, setMode] = useState(true);
+
+  const toggleMode = () => {
+    localStorage.setItem("mode", !mode);
+    setMode(!mode);
+  };
+
+  useEffect(() => {
+    const mode = localStorage.getItem("mode");
+    if (mode !== null) {
+      setMode(JSON.parse(mode));
+    }
+  }, []);
 
   const toggleSettings = () => {
     setDisplaySettings(!displaySettings);
   };
   return (
     <>
-      <Navbar toggleSettings={toggleSettings} />
+      <Navbar
+        toggleSettings={toggleSettings}
+        mode={mode}
+        toggleMode={toggleMode}
+      />
       <TimerContainer displaySettings={displaySettings} />
     </>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import "./Navbar.scss";
 import Logo from "../../assets/tomato.svg?react";
+import Sun from "../../assets/icons/sun.svg?react";
+import Moon from "../../assets/icons/moon.svg?react";
 
-function Navbar({ toggleSettings }) {
+function Navbar({ toggleSettings, mode, toggleMode }) {
   return (
     <header className="header">
-      <nav className="nav-bar">
+      <nav className={`nav-bar ${mode ? "nav-bar--light" : "nav-bar--dark"}`}>
         <ul className="nav-list">
           <li>
             <a href="/" className="nav-list__link">
@@ -12,8 +14,29 @@ function Navbar({ toggleSettings }) {
               Customato
             </a>
           </li>
+          <li className="nav-list__left-item">
+            <button
+              className={`nav-list__toggle-btn ${
+                mode
+                  ? "nav-list__toggle-btn--light"
+                  : "nav-list__toggle-btn--dark"
+              }`}
+              onClick={toggleMode}
+            >
+              {mode ? (
+                <Sun className="nav-list__svg" />
+              ) : (
+                <Moon className="nav-list__svg" />
+              )}
+            </button>
+          </li>
           <li>
-            <button className="nav-list__btn" onClick={toggleSettings}>
+            <button
+              className={`nav-list__btn ${
+                mode ? "nav-list__btn--light" : "nav-list__btn--dark"
+              }`}
+              onClick={toggleSettings}
+            >
               Settings
             </button>
           </li>
